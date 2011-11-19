ca9f891c5d70a99c83b074e38d6adf7c2c7a0230
Jayson Reis <santosdosreis@gmail.com>
2011-11-19 13:36:10 -0200

    authentication done

--- bmemcached/__init__.py
+++ bmemcached/__init__.py
@@ -1,7 +1,9 @@
 import struct
 import socket
+import logging
 
 __all__ = ['Client']
+logger = logging.getLogger('bmemcached')
 
 
 class Client(object):
@@ -14,6 +16,7 @@ class Client(object):
 
 class Server(object):
     HEADER_STRUCT = '!BBHBBHLLQ'
+    HEADER_SIZE = 24
 
     MAGIC = {
         'request': 0x80,
@@ -28,16 +31,70 @@ class Server(object):
         'auth_request': {'command': 0x21, 'struct': '%ds%ds'}
     }
 
+    STATUS = {
+        'success': 0x00,
+        'unknown_command': 0x81
+    }
+
     def __init__(self, server, username=None, password=None):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server = server.split(':')
         host = server[0]
-        if len(server) >1:
+        if len(server) > 1:
             try:
                 port = int(server[1])
             except (ValueError, TypeError):
                 port = 11211
         else:
             port = 11211
-        self.connection.connect((host, port))
 
+        self.connection.connect((host, port))
+        if username and password:
+            self.authenticate(username, password)
+
+    def authenticate(self, username, password):
+        logger.info('Authenticating as %s' % username)
+        self.connection.send(struct.pack(self.HEADER_STRUCT,
+            self.MAGIC['request'],
+            self.COMMANDS['auth_negotiation']['command'],
+            0, 0, 0, 0, 0, 0, 0))
+        header = self.connection.recv(self.HEADER_SIZE)
+        (magic, opcode, keylen, extlen, datatype, status, bodylen, opaque,
+            cas) = struct.unpack(self.HEADER_STRUCT, header)
+
+        if status == self.STATUS['unknown_command']:
+            logger.debug('Server does not requires authentication.')
+            return True
+
+        methods = self.connection.recv(bodylen).split(' ')
+
+        if not 'PLAIN' in methods:
+            raise AuthenticationNotSupported('This module only supports ' + \
+                'PLAIN auth for now.')
+
+        method = 'PLAIN'
+        auth = '\x00%s\x00%s' % (username, password)
+        self.connection.send(struct.pack(self.HEADER_STRUCT + \
+            self.COMMANDS['auth_request']['struct'] % (len(method), len(auth)),
+            self.MAGIC['request'], self.COMMANDS['auth_request']['command'],
+            len(method), 0, 0, 0, len(method) + len(auth), 0, 0, method, auth))
+        headers = self.connection.recv(self.HEADER_SIZE)
+        (magic, opcode, keylen, extlen, datatype, status, bodylen,
+            opaque, cas) = struct.unpack(self.HEADER_STRUCT, headers)
+
+        if status != self.STATUS['success']:
+            raise MemcachedException('Code: %d Message: %s' % (status,
+                self.connection.recv(bodylen)))
+
+        logger.debug('Auth OK. Code: %d Message: %s' % (status,
+            self.connection.recv(bodylen)))
+
+        return True
+
+
+class AuthenticationNotSupported(Exception):
+    pass
+
+
+class MemcachedException(Exception):
+    pass
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,6 @@
 import unittest
 import bmemcached
+import os
 
 
 class MainTests(unittest.TestCase):
@@ -12,3 +13,14 @@ class MainTests(unittest.TestCase):
     def testGet(self):
         self.client.set('test', 'test')
         self.assertEqual('test', self.client.get('test'))
+
+
+class TestAuthentication(unittest.TestCase):
+    def setUp(self):
+        self.server = bmemcached.Server(os.environ.get('MEMCACHED_SERVER',
+            '127.0.0.1'))
+
+    def testAuth(self):
+        self.assertTrue(self.server.authenticate(
+            os.environ.get('MEMCACHED_USERNAME', 'user'),
+            os.environ.get('MEMCACHED_PASSWORD', 'password')))
