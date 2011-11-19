558edf2890881c4ecd9e424a7c94638b53436531
Jayson Reis <santosdosreis@gmail.com>
2011-11-19 12:53:25 -0200

    basic connections stuff

--- bmemcached/__init__.py
+++ bmemcached/__init__.py
@@ -7,10 +7,10 @@ __all__ = ['Client']
 class Client(object):
     def __init__(self, servers):
         self.set_servers(servers)
-    
+
     def set_servers(self, servers):
         self.servers = [Server(server) for server in servers]
-    
+
 
 class Server(object):
     HEADER_STRUCT = '!BBHBBHLLQ'
@@ -20,6 +20,7 @@ class Server(object):
         'response': 0x81
     }
 
+    # All structures will be appended to HEADER_STRUCT
     COMMANDS = {
         'get': {'command': 0x00, 'struct': '%ds'},
         'set': {'command': 0x01},
@@ -27,5 +28,16 @@ class Server(object):
         'auth_request': {'command': 0x21, 'struct': '%ds%ds'}
     }
 
-    def __init__(self, server):
-        pass
+    def __init__(self, server, username=None, password=None):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server = server.split(':')
+        host = server[0]
+        if len(server) >1:
+            try:
+                port = int(server[1])
+            except (ValuError, TypeError):
+                port = 11211
+        else:
+            port = 11211
+        self.connection.connect((host, port))
+
--- tests/__init__.py
+++ tests/__init__.py
@@ -5,10 +5,10 @@ import bmemcached
 class MainTests(unittest.TestCase):
     def setUp(self):
         self.client = bmemcached.Client(('127.0.0.1:11211', ))
-    
+
     def testSet(self):
         self.assertEqual(0, self.client.set('test', 'test'))
-    
+
     def testGet(self):
         self.client.set('test', 'test')
         self.assertEqual('test', self.client.get('test'))
