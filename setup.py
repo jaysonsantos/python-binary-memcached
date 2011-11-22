8803e653c6e5e172090bca323ca981b404ef5ef7
Jayson Reis <santosdosreis@gmail.com>
2011-11-22 14:39:12 -0200

    bump to 0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='python-binary-memcached',
-    version='0.3',
+    version='0.4',
     author='Jayson Reis',
     author_email='santosdosreis@gmail.com',
     description='A pure python module to access memcached via it\'s binary' + \
