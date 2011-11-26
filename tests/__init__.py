de3375ed283047632c3167716c10d5cec1159fcc
Eric Davis <ed@npri.org>
2011-11-25 18:55:34 -0800

    Drop test for MEMCACHED_* authentication

--- tests/__init__.py
+++ tests/__init__.py
@@ -99,17 +99,6 @@ class MainTests(unittest.TestCase):
         self.assertTrue('1:get_hits' in stats)
 
 
-class TestAuthentication(unittest.TestCase):
-    def setUp(self):
-        self.server = bmemcached.Server(os.environ.get('MEMCACHED_SERVER',
-            '127.0.0.1'))
-
-    def testAuth(self):
-        self.assertTrue(self.server.authenticate(
-            os.environ.get('MEMCACHED_USERNAME', 'user'),
-            os.environ.get('MEMCACHED_PASSWORD', 'password')))
-
-
 class TestServerParsing(unittest.TestCase):
     def testAcceptStringServer(self):
         client = bmemcached.Client('127.0.0.1:11211')
