3e83b6c0cedd0756191fff4638cae5426ae647fc
Jayson Reis <santosdosreis@gmail.com>
2012-02-27 22:49:15 -0300

    typo on commands

--- .travis-runs-tests.sh
+++ .travis-runs-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo service memcached stop
 memcached -S -d
-memached -s/tmp/memached.sock -d
-memached -p5000 -d
+memcached -s/tmp/memached.sock -d
+memcached -p5000 -d
 
 nosetests
 exit $?
