ace31ac6d2f11bda1d7d912d1d2db6a3414442e7
Jayson Reis <santosdosreis@gmail.com>
2013-04-14 23:47:56 -0300

    Some PEP8

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     version='0.16',
     author='Jayson Reis',
     author_email='santosdosreis@gmail.com',
-    description='A pure python module to access memcached via it\'s binary' + \
-        ' protocol with SASL auth support',
+    description='A pure python module to access memcached via it\'s binary' +
+                ' protocol with SASL auth support',
     url='https://github.com/jaysonsantos/python-binary-memcached',
     packages=['bmemcached'],
     classifiers=[
