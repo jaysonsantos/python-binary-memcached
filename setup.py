ba03e1ba2d30efce963cf714c84a5f3bb34caf6e
Jayson Reis (jaysonsantos) <santosdosreis@gmail.com>
2015-07-31 23:17:17 +0200

    Bump to add classifiers

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='python-binary-memcached',
-    version='0.24.4',
+    version='0.24.5',
     author='Jayson Reis',
     author_email='santosdosreis@gmail.com',
     description='A pure python module to access memcached via it\'s binary protocol with SASL auth support',
