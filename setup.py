fc307637acd20389c4c4c7483b16fdaee73103bf
Jayson Reis (jaysonsantos) <santosdosreis@gmail.com>
2015-07-31 23:14:34 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
     ],
     install_requires=[
         'six'
