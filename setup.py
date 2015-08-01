69c0bba3cce856518e2bfe281ad01506c2e31f10
requires.io <support@requires.io>
2015-08-01 23:57:56 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup
 setup(
     name='python-binary-memcached',
-    version='0.24.3',
+    version='0.24.5',
     author='Jayson Reis',
     author_email='santosdosreis@gmail.com',
-    description='A pure python module (thread safe) to access memcached via it\'s binary' +
-                ' protocol with SASL auth support',
+    description='A pure python module to access memcached via it\'s binary protocol with SASL auth support',
     url='https://github.com/jaysonsantos/python-binary-memcached',
     packages=['bmemcached'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+    install_requires=[
+        'six'
     ]
 )
