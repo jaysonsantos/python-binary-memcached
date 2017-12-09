5e8ef2db232313ee596645a52aea4e5add826ac2
Jayson Reis <santosdosreis@gmail.com>
2017-12-09 09:05:07 +0100

    Add changelog and readme to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+import os
 from setuptools import setup
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
 setup(
     name='python-binary-memcached',
     version='0.26.1',
     author='Jayson Reis',
     author_email='santosdosreis@gmail.com',
     description='A pure python module to access memcached via its binary protocol with SASL auth support',
+    long_description='{0}\n{1}'.format(read('README.rst'), read('CHANGELOG.rst')),
     url='https://github.com/jaysonsantos/python-binary-memcached',
     packages=['bmemcached', 'bmemcached.client'],
     classifiers=[
