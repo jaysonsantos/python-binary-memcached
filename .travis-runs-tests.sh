3a77cff969d8df9b0f6e199615093a0b2e6bddab
Jayson Reis <santosdosreis@gmail.com>
2018-08-10 09:41:03 +0200

    chore: Add support for linting in travis

--- .travis-runs-tests.sh
+++ .travis-runs-tests.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
-sudo service memcached stop
-py.test --version
-export PYTHONPATH=.
-python setup.py develop
-py.test --cov=bmemcached
-exit $?
+#!/bin/bash -e
+if [ "$STEP" -eq "tests" ]; then
+    sudo service memcached stop
+    py.test --version
+    export PYTHONPATH=.
+    python setup.py develop
+    py.test --cov=bmemcached
+fi
+
+if [ "$STEP" -eq "lint" ]; then
+    flake8
+fi
