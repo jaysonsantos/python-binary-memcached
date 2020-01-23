c45d689631601b654f976a54376948e37a8c01fc
Moisés Guimarães de Medeiros <moguimar@redhat.com>
2020-01-23 16:55:11 +0100

    Uses trustme instead of hardcoded certificates.
    Closes: #225

--- test/test_tls.py
+++ test/test_tls.py
@@ -3,38 +3,45 @@ import pytest
 import subprocess
 import ssl
 import time
+import trustme
 
 import bmemcached
 import test_simple_functions
 
 
+ca = trustme.CA()
+server_cert = ca.issue_cert(os.environ["MEMCACHED_HOST"] + u"")
+
+
 @pytest.yield_fixture(scope="module", autouse=True)
 def memcached_tls():
-    p = subprocess.Popen(
-        [
-            "memcached",
-            "-p5001",
-            "-Z",
-            "-o",
-            "ssl_chain_cert=test/certs/gen/chain/server-rsa2048.pem",
-            "-o",
-            "ssl_key=test/certs/gen/key/server-rsa2048.key",
-            "-o",
-            "ssl_ca_cert=test/certs/gen/crt/client-ca-root.crt",
-            "-o",
-            "ssl_verify_mode=1",
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    time.sleep(0.1)
+    key = server_cert.private_key_pem
+    cert = server_cert.cert_chain_pems[0]
 
-    if p.poll() is not None:
-        pytest.skip("Memcached server is not built with TLS support.")
+    with cert.tempfile() as c, key.tempfile() as k:
+        p = subprocess.Popen(
+            [
+                "memcached",
+                "-p5001",
+                "-Z",
+                "-o",
+                "ssl_key={}".format(k),
+                "-o",
+                "ssl_chain_cert={}".format(c),
+                "-o",
+                "ssl_verify_mode=1",
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        time.sleep(0.1)
 
-    yield p
-    p.kill()
-    p.wait()
+        if p.poll() is not None:
+            pytest.skip("Memcached server is not built with TLS support.")
+
+        yield p
+        p.kill()
+        p.wait()
 
 
 class TLSMemcachedTests(test_simple_functions.MemcachedTests):
@@ -43,9 +50,9 @@ class TLSMemcachedTests(test_simple_functions.MemcachedTests):
     """
 
     def setUp(self):
-        ctx = ssl.create_default_context(
-            cafile="test/certs/gen/crt/ca-root.crt"
-        )
+        ctx = ssl.create_default_context()
+
+        ca.configure_trust(ctx)
 
         self.server = "{}:5001".format(os.environ["MEMCACHED_HOST"])
         self.client = bmemcached.Client(self.server, tls_context=ctx)
