600ec1bceff5341d9bb2b7748c4f2b80cbc9f56f
Jayson Reis <santosdosreis@gmail.com>
2024-10-23 14:39:50 +0200

    bump: version 0.31.2 → 0.31.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ version_dependant_requirements = [
 
 setup(
     name="python-binary-memcached",
-    version="0.31.2",
+    version="0.31.3",
     author="Jayson Reis",
     author_email="santosdosreis@gmail.com",
     description="A pure python module to access memcached via its binary protocol with SASL auth support",
